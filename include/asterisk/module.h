36b321e380a18c6a700f3b2bedb6b80d8a3fd5ab
Automerge script <automerge@asterisk.org>
2006-04-11 16:02:39 +0000

    automerge commit
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/astum@19238 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- include/asterisk/module.h
+++ include/asterisk/module.h
@@ -260,28 +260,6 @@ int ast_module_reload(const char *name);
  */
 char *ast_module_helper(const char *line, const char *word, int pos, int state, int rpos, int needsreload);
 
-/*! 
- * \brief Register a function to be executed before Asterisk exits.
- * \param func The callback function to use.
- *
- * \return Zero on success, -1 on error.
- */
-int ast_register_atexit(void (*func)(void));
-
-/*! 
- * \brief Unregister a function registered with ast_register_atexit().
- * \param func The callback function to unregister.
- */
-void ast_unregister_atexit(void (*func)(void));
-
-/*!
- * \brief Given a function address, find the corresponding module.
- * This is required as a workaround to the fact that we do not
- * have a module argument to the load_module() function.
- * Hopefully the performance implications are small.
- */
-struct module *ast_find_module(int (*load_fn)(void));
-
 /* Local user routines keep track of which channels are using a given module
    resource.  They can help make removing modules safer, particularly if
    they're in use at the time they have been requested to be removed */
