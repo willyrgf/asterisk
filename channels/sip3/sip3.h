1068207c319d64bad39569b86ff6e6914df92abb
Olle Johansson <oej@edvina.net>
2006-10-07 21:03:08 +0000

    Adding .h file
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/codename-pineapple@44741 65c4cc65-6c06-0410-ace0-fbb531ad65f3

---
+++ channels/sip3/sip3.h
@@ -0,0 +1,23 @@
+/*
+ * Asterisk -- A telephony toolkit for Linux.
+ *
+ * Implementation of SIP - The Session Initiation protocol
+ *
+ * Version 3 of the SIP channel
+ * 
+ * Copyright (C) 2003-2006, Digium Inc
+ * and Edvina AB (for the chan_sip3 additions/changes)
+ *
+ * Mark Spencer <markster@linux-support.net>
+ * and Olle E. Johansson <oej@edvina.net>
+ *
+ * This program is free software, distributed under the terms of
+ * the GNU General Public License
+ */
+ 
+#ifndef _SIP3_H
+#define _SIP3_H
+
+#define THIS_IS_A_TEST	"Pineapples are sweet"
+
+#endif
