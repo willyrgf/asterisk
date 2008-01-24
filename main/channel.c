c65f213472355d8da30a26d28921a68f75a7fcbe
Automerge script <automerge@asterisk.org>
2008-01-24 04:14:03 +0000

    automerge commit
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/astum@100104 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -206,10 +206,12 @@ static char *handle_cli_core_show_channeltypes(struct ast_cli_entry *e, int cmd,
 
 	ast_cli(a->fd, FORMAT, "Type", "Description",       "Devicestate", "Indications", "Transfer");
 	ast_cli(a->fd, FORMAT, "----------", "-----------", "-----------", "-----------", "--------");
+
 	if (AST_RWLIST_RDLOCK(&channels)) {
 		ast_log(LOG_WARNING, "Unable to lock channel list\n");
 		return CLI_FAILURE;
 	}
+
 	AST_LIST_TRAVERSE(&backends, cl, list) {
 		ast_cli(a->fd, FORMAT, cl->tech->type, cl->tech->description,
 			(cl->tech->devicestate) ? "yes" : "no",
@@ -217,8 +219,11 @@ static char *handle_cli_core_show_channeltypes(struct ast_cli_entry *e, int cmd,
 			(cl->tech->transfer) ? "yes" : "no");
 		count_chan++;
 	}
+
 	AST_RWLIST_UNLOCK(&channels);
+
 	ast_cli(a->fd, "----------\n%d channel drivers registered.\n", count_chan);
+
 	return CLI_SUCCESS;
 
 #undef FORMAT
@@ -605,10 +610,13 @@ int ast_best_codec(int fmts)
 	fmts &= AST_FORMAT_AUDIO_MASK;
 	
 	/* Find the first preferred codec in the format given */
-	for (x=0; x < (sizeof(prefs) / sizeof(prefs[0]) ); x++)
+	for (x = 0; x < (sizeof(prefs) / sizeof(prefs[0]) ); x++) {
 		if (fmts & prefs[x])
 			return prefs[x];
+	}
+
 	ast_log(LOG_WARNING, "Don't know any of 0x%x formats\n", fmts);
+
 	return 0;
 }
 
