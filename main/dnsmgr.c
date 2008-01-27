a3f6a76c1d8106c0ffe2b9f74052ae17b31e5541
Automerge script <automerge@asterisk.org>
2008-01-27 22:15:48 +0000

    automerge commit
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/moremanager@100478 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/dnsmgr.c
+++ main/dnsmgr.c
@@ -253,7 +253,7 @@ static int refresh_list(const void *data)
 void dnsmgr_start_refresh(void)
 {
 	if (refresh_sched > -1) {
-		ast_sched_del(sched, refresh_sched);
+		AST_SCHED_DEL(sched, refresh_sched);
 		refresh_sched = ast_sched_add_variable(sched, 100, refresh_list, &master_refresh_info, 1);
 	}
 }
@@ -368,8 +368,7 @@ static int do_reload(int loading)
 	was_enabled = enabled;
 	enabled = 0;
 
-	if (refresh_sched > -1)
-		ast_sched_del(sched, refresh_sched);
+	AST_SCHED_DEL(sched, refresh_sched);
 
 	if ((config = ast_config_load("dnsmgr.conf"))) {
 		if ((enabled_value = ast_variable_retrieve(config, "general", "enable"))) {
