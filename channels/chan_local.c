c00ce7f5bf4a26189a6393ad55304c2cfdd4415d
Automerge script <automerge@asterisk.org>
2008-04-28 15:57:52 +0000

    automerge commit
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/obproxy@114750 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_local.c
+++ channels/chan_local.c
@@ -471,6 +471,7 @@ static int local_call(struct ast_channel *ast, char *dest, int timeout)
 	p->chan->cid.cid_pres = p->owner->cid.cid_pres;
 	ast_string_field_set(p->chan, language, p->owner->language);
 	ast_string_field_set(p->chan, accountcode, p->owner->accountcode);
+	ast_cdr_update(p->chan);
 	p->chan->cdrflags = p->owner->cdrflags;
 
 	/* copy the channel variables from the incoming channel to the outgoing channel */
@@ -514,7 +515,13 @@ static int local_hangup(struct ast_channel *ast)
 			/* Deadlock avoidance */
 			while (p->owner && ast_channel_trylock(p->owner)) {
 				ast_mutex_unlock(&p->lock);
+				if (ast) {
+					ast_channel_unlock(ast);
+				}
 				usleep(1);
+				if (ast) {
+					ast_channel_lock(ast);
+				}
 				ast_mutex_lock(&p->lock);
 			}
 			if (p->owner) {
