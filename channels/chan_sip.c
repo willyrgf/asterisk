ae8339873d738df97a3cd2013c37d27548a7470c
Automerge script <automerge@asterisk.org>
2008-09-18 22:26:50 +0000

    Merged revisions 143534 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
    r143534 | murf | 2008-09-18 17:11:51 -0500 (Thu, 18 Sep 2008) | 1 line
    
    A micro-fix, in sip_park_thread, where d is freed before the func is done using it.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/sipchanstats@143548 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -13322,7 +13322,6 @@ static void *sip_park_thread(void *stuff)
 	transferee = d->chan1;
 	transferer = d->chan2;
 	copy_request(&req, &d->req);
-	free(d);
 
 	if (!transferee || !transferer) {
 		ast_log(LOG_ERROR, "Missing channels for parking! Transferer %s Transferee %s\n", transferer ? "<available>" : "<missing>", transferee ? "<available>" : "<missing>" );
@@ -13371,6 +13370,7 @@ static void *sip_park_thread(void *stuff)
 			ast_log(LOG_DEBUG, "SIP Call parked failed \n");
 		/* Do not hangup call */
 	}
+	free(d);
 	return NULL;
 }
 
