6866a34380cda2fdc71209b17fd2d34d8e90bd95
Automerge script <automerge@asterisk.org>
2008-12-02 23:58:13 +0000

    Merged revisions 160390,160411 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
    r160390 | twilson | 2008-12-02 17:04:23 -0600 (Tue, 02 Dec 2008) | 6 lines
    
    A situation like A calls B, A builtin_atxfers B to C, C parks B would lead to a crash.  Thanks to file for telling me how to fix it!
    
    (closes issue #13854)
    Reported by: Adam Lee
    Tested by: otherwiseguy
    
    ........
    r160411 | twilson | 2008-12-02 17:58:08 -0600 (Tue, 02 Dec 2008) | 2 lines
    
    Channel is masqueraded, don't keep alive
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/sipchanstats@160412 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_features.c
+++ res/res_features.c
@@ -752,11 +752,11 @@ static int builtin_blindtransfer(struct ast_channel *chan, struct ast_channel *p
 		res = finishup(transferee);
 		if (res)
 			res = -1;
-		else if (!ast_park_call(transferee, transferer, 0, NULL)) {	/* success */
+		else if (!masq_park_call_announce(transferee, transferer, 0, NULL)) {	/* success */
 			/* We return non-zero, but tell the PBX not to hang the channel when
 			   the thread dies -- We have to be careful now though.  We are responsible for 
 			   hanging up the channel, else it will never be hung up! */
-			return (transferer == peer) ? AST_PBX_KEEPALIVE : AST_PBX_NO_HANGUP_PEER_PARKED;
+			return 0;
 		} else {
 			ast_log(LOG_WARNING, "Unable to park call %s\n", transferee->name);
 		}
