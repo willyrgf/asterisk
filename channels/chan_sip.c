1a70fa4fb5585dd25b69a5056a55e80a5557d326
Automerge script <automerge@asterisk.org>
2008-12-10 17:17:56 +0000

    Merged revisions 162653,162659,162663,162670-162671 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
    r162653 | file | 2008-12-10 10:05:29 -0600 (Wed, 10 Dec 2008) | 6 lines
    
    Increment the sequence number on the end packets for RFC2833. After reading the RFC some more and doing some testing I agree with this change.
    (closes issue #12983)
    Reported by: vt
    Patches:
          dtmf_inc_seqnum_on_end_pkts.diff uploaded by vt (license 520)
    
    ........
    r162659 | mmichelson | 2008-12-10 10:10:25 -0600 (Wed, 10 Dec 2008) | 8 lines
    
    Add missing documentation to misdn.txt
    
    (closes issue #14052)
    Reported by: festr
    Patches:
          misdn.txt.patch uploaded by festr (license 443)
    
    
    ........
    r162663 | mmichelson | 2008-12-10 10:24:56 -0600 (Wed, 10 Dec 2008) | 11 lines
    
    Revert fix for issue 13570. It has caused more problems than
    it helped to fix.
    
    (closes issue #13783)
    Reported by: navkumar
    
    
    (closes issue #14025)
    Reported by: ffs
    
    
    ........
    r162670 | mmichelson | 2008-12-10 10:44:37 -0600 (Wed, 10 Dec 2008) | 14 lines
    
    Update to stringfield handling so that side-effects on
    parameters are not evaluated multiple times.
    
    An example where this caused a problem was in chan_sip.c, with
    the line
    
      ast_string_field_set(p, fromdomain, ++fromdomain);
    
    This patch was originally uploaded to issue #13783 by
    jamessan. While the issue was closed for other reasons, this
    patch is valid and fixes a separate problem, and is thus
    being committed.
    
    
    ........
    r162671 | murf | 2008-12-10 10:45:01 -0600 (Wed, 10 Dec 2008) | 22 lines
    
    (closes issue #14022)
    Reported by: wetwired
    Tested by: murf
    
    I checked, and I added a mod to the trunk version
    of Asterisk that would make it 8-bit transparent
    on 27 Nov 2007, but I made no such updates to
    1.4. My best guess is that 1.4 was released, and
    it was not appropriate to commit an enhancement.
    
    But I'm going to add the same fixes to 1.4 now,
    for the following reasons:
    1. wetwired is correct; 1.4 is **mostly** 8-bit
       transparent now. This is because the lexical
       token forming rules use . in most 'word'
       state continuances.  It's just the beginning
       of a 'word' that is picky.
    2. Accepting 8-bit chars in some places and
       not others leads to bug reports like this.
    
    
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/sipchanstats@162714 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -167,8 +167,6 @@ ASTERISK_FILE_VERSION(__FILE__, "$Revision$")
 #define IPTOS_MINCOST           0x02
 #endif
 
-#define SIP_RESERVED ";/?:@&=+$,# "
-
 /* #define VOCAL_DATA_HACK */
 
 #define DEFAULT_DEFAULT_EXPIRY  120
@@ -4750,11 +4748,9 @@ static int sip_register(char *value, int lineno)
 	struct sip_registry *reg;
 	int portnum = 0;
 	char username[256] = "";
-	char *user;
 	char *hostname=NULL, *secret=NULL, *authuser=NULL;
 	char *porta=NULL;
 	char *contact=NULL;
-	char *reserved = NULL;
 
 	if (!value)
 		return -1;
@@ -4775,16 +4771,6 @@ static int sip_register(char *value, int lineno)
 		if (authuser)
 			*authuser++ = '\0';
 	}
-	user = username;
-	if ((reserved = strpbrk(user, SIP_RESERVED))) {
-		goto invalid_char;
-	}
-	if (!ast_strlen_zero(secret) && (reserved = strpbrk(secret, SIP_RESERVED))) {
-		goto invalid_char;
-	}
-	if (!ast_strlen_zero(authuser) && (reserved = strpbrk(authuser, SIP_RESERVED))) {
-		goto invalid_char;
-	}
 	/* split host[:port][/contact] */
 	contact = strchr(hostname, '/');
 	if (contact)
@@ -4799,9 +4785,6 @@ static int sip_register(char *value, int lineno)
 			ast_log(LOG_WARNING, "%s is not a valid port number at line %d\n", porta, lineno);
 			return -1;
 		}
-	}
-	if ((reserved = strpbrk(hostname, SIP_RESERVED))) {
-		goto invalid_char;
 	}
 	if (!(reg = ast_calloc(1, sizeof(*reg)))) {
 		ast_log(LOG_ERROR, "Out of memory. Can't allocate SIP registry entry\n");
@@ -4834,10 +4817,6 @@ static int sip_register(char *value, int lineno)
 	ASTOBJ_CONTAINER_LINK(&regl, reg);	/* Add the new registry entry to the list */
 	ASTOBJ_UNREF(reg,sip_registry_destroy);
 	return 0;
-
-invalid_char:
-	ast_log(LOG_ERROR, "A reserved character ('%c') was used in a \"register\" line. This registration will not occur\n", *reserved);
-	return -1;
 }
 
 /*! \brief  Parse multiline SIP headers into one header
