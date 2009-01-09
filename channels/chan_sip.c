cfb7a75e758f6ec0c8705c0b3cac508db60e0027
Automerge script <automerge@asterisk.org>
2009-01-09 20:25:00 +0000

    Merged revisions 168128 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
    r168128 | mmichelson | 2009-01-09 14:08:04 -0600 (Fri, 09 Jan 2009) | 13 lines
    
    Add check_via calls to more request handlers
    
    INFO, NOTIFY, OPTIONS, REFER, and MESSAGE requests
    were not checking the topmost Via to determine where
    to send the response. Adding check_via calls to those
    request handlers solves this.
    
    (closes issue #13071)
    Reported by: baron
    Patches:
          check_via.patch uploaded by baron (license 531)
    Tested by: baron
    
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/obproxy@168141 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -11448,6 +11448,7 @@ static void handle_request_info(struct sip_pvt *p, struct sip_request *req)
 	unsigned int event;
 	const char *c = get_header(req, "Content-Type");
 
+	check_via(p, req);
 	/* Need to check the media/type */
 	if (!strcasecmp(c, "application/dtmf-relay") ||
 	    !strcasecmp(c, "application/vnd.nortelnetworks.digits")) {
@@ -13696,6 +13697,7 @@ static int handle_request_notify(struct sip_pvt *p, struct sip_request *req, str
 	char *eventid = NULL;
 	char *sep;
 
+	check_via(p, req);
 	if( (sep = strchr(event, ';')) ) {	/* XXX bug here - overwriting string ? */
 		*sep++ = '\0';
 		eventid = sep;
@@ -13823,7 +13825,7 @@ static int handle_request_options(struct sip_pvt *p, struct sip_request *req)
 {
 	int res;
 
-
+	check_via(p, req);
 	/* XXX Should we authenticate OPTIONS? XXX */
 
 	if (p->lastinvite) {
@@ -15019,6 +15021,7 @@ static int handle_request_refer(struct sip_pvt *p, struct sip_request *req, int 
 
 	int res = 0;
 
+	check_via(p, req);
 	if (ast_test_flag(req, SIP_PKT_DEBUG))
 		ast_verbose("Call %s got a SIP call transfer from %s: (REFER)!\n", p->callid, ast_test_flag(&p->flags[0], SIP_OUTGOING) ? "callee" : "caller");
 
@@ -15469,6 +15472,7 @@ static int handle_request_bye(struct sip_pvt *p, struct sip_request *req)
 static int handle_request_message(struct sip_pvt *p, struct sip_request *req)
 {
 	if (!ast_test_flag(req, SIP_PKT_IGNORE)) {
+		check_via(p, req);
 		if (ast_test_flag(req, SIP_PKT_DEBUG))
 			ast_verbose("Receiving message!\n");
 		receive_message(p, req);
