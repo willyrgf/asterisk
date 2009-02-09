34adb62d47435d7979022add50c622e1d2ba9ad1
Automerge script <automerge@asterisk.org>
2009-02-09 17:20:29 +0000

    Merged revisions 174282 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
      r174282 | mmichelson | 2009-02-09 11:11:05 -0600 (Mon, 09 Feb 2009) | 12 lines
      
      Don't do an SRV lookup if a port is specified
      
      RFC 3263 says to do A record lookups on a hostname
      if a port has been specified, so that's what we're
      going to do. See section 4.2.
      
      (closes issue #14419)
      Reported by: klaus3000
      Patches:
            patch_chan_sip_nosrvifport_1.4.23.txt uploaded by klaus3000 (license 65)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/obproxy@174299 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -3017,7 +3017,7 @@ static int create_addr(struct sip_pvt *dialog, const char *opeer, struct sockadd
 	struct ast_hostent ahp;
 	struct sip_peer *p;
 	char *port;
-	int portno;
+	int portno = 0;
 	char host[MAXHOSTNAMELEN], *hostn;
 	char peer[256];
 
@@ -3062,8 +3062,10 @@ static int create_addr(struct sip_pvt *dialog, const char *opeer, struct sockadd
 		}
 	} else {
 		hostn = peer;
-		portno = port ? atoi(port) : STANDARD_SIP_PORT;
-		if (srvlookup) {
+ 		/* Section 4.2 of RFC 3263 specifies that if a port number is specified, then
+		 * an A record lookup should be used instead of SRV.
+		 */
+		if (!port && srvlookup) {
 			char service[MAXHOSTNAMELEN];
 			int tportno;
 			int ret;
@@ -3075,6 +3077,9 @@ static int create_addr(struct sip_pvt *dialog, const char *opeer, struct sockadd
 				portno = tportno;
 			}
 		}
+	 	if (!portno)
+	 		portno = port ? atoi(port) : STANDARD_SIP_PORT;
+	 
 		hp = ast_gethostbyname(hostn, &ahp);
 		if (!hp) {
 			ast_log(LOG_WARNING, "No such host: %s\n", peer);
