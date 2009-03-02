5999e2f3e93d689b3a63fbee2fcbe2e5c29aa49e
Automerge script <automerge@asterisk.org>
2009-03-02 23:24:57 +0000

    Merged revisions 179461,179468 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
      r179461 | russell | 2009-03-02 16:58:18 -0600 (Mon, 02 Mar 2009) | 8 lines
      
      Ensure that only one thread is calling ast_settimeout() on a channel at a time.
      
      For example, with an IAX2 channel, you can have both the channel thread and the
      chan_iax2 processing threads calling this function, and doing so twice at the
      same time is a bad thing.
      
      (Found in a debugging session with dvossel and mmichelson)
    ........
      r179468 | tilghman | 2009-03-02 17:09:01 -0600 (Mon, 02 Mar 2009) | 10 lines
      
      When ending a recording with silence detection, remember to reduce the duration.
      The end of the recording is correspondingly trimmed, but the duration was not
      trimmed by the number of seconds trimmed, so the saved duration was necessarily
      longer than the actual soundfile duration.
      (closes issue #14406)
       Reported by: sasargen
       Patches: 
             20090226__bug14406.diff.txt uploaded by tilghman (license 14)
       Tested by: sasargen
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/cancel_elsewhere_1.4@179508 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -1843,6 +1843,7 @@ int ast_settimeout(struct ast_channel *c, int samples, int (*func)(const void *d
 {
 	int res = -1;
 #ifdef HAVE_DAHDI
+	ast_channel_lock(c);
 	if (c->timingfd > -1) {
 		if (!func) {
 			samples = 0;
@@ -1854,6 +1855,7 @@ int ast_settimeout(struct ast_channel *c, int samples, int (*func)(const void *d
 		c->timingfunc = func;
 		c->timingdata = data;
 	}
+	ast_channel_unlock(c);
 #endif	
 	return res;
 }
