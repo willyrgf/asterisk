70b45cf5aa7719dea613f1c89ba6cebc1bef2e7a
Automerge script <automerge@asterisk.org>
2009-09-16 20:25:19 +0000

    Merged revisions 218918,218933 via svnmerge from file:///srv/subversion/repos/asterisk/trunk
    ........
      r218918 | file | 2009-09-16 13:31:47 -0500 (Wed, 16 Sep 2009) | 5 lines
      
      On TCP and TLS connections do not attempt to stop retransmission of the packet internally.
      
      This was preventing responses from being properly processed because the packet was not being found
      causing handle_response to return prematurely.
    ........
      r218933 | mmichelson | 2009-09-16 14:25:36 -0500 (Wed, 16 Sep 2009) | 12 lines
      
      Reverse order of args to fread.
      
      This way, we don't always write a null byte into
      byte 1 of the buffer
      
      (closes issue #15905)
      Reported by: ebroad
      Patches:
            freadfix.patch uploaded by ebroad (license 878)
      Tested by: ebroad
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/pinetree-trunk@218962 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -2972,7 +2972,7 @@ static void *_sip_tcp_helper_thread(struct sip_pvt *pvt, struct ast_tcptls_sessi
 			while (cl > 0) {
 				size_t bytes_read;
 				ast_mutex_lock(&tcptls_session->lock);
-				if (!(bytes_read = fread(buf, MIN(sizeof(buf) - 1, cl), 1, tcptls_session->f))) {
+				if (!(bytes_read = fread(buf, 1, MIN(sizeof(buf) - 1, cl), tcptls_session->f))) {
 					ast_mutex_unlock(&tcptls_session->lock);
 					goto cleanup;
 				}
@@ -18850,7 +18850,6 @@ static void handle_response(struct sip_pvt *p, int resp, const char *rest, struc
 	struct ast_channel *owner;
 	int sipmethod;
 	int res = 1;
-	int ack_res;
 	const char *c = get_header(req, "Cseq");
 	/* GCC 4.2 complains if I try to cast c as a char * when passing it to ast_skip_nonblanks, so make a copy of it */
 	char *c_copy = ast_strdupa(c);
@@ -18872,16 +18871,20 @@ static void handle_response(struct sip_pvt *p, int resp, const char *rest, struc
 		pbx_builtin_setvar_helper(owner, causevar, causeval);
 	}
 
-	/* Acknowledge whatever it is destined for */
-	if ((resp >= 100) && (resp <= 199)) {
-		ack_res = __sip_semi_ack(p, seqno, 0, sipmethod);
-	} else {
-		ack_res = __sip_ack(p, seqno, 0, sipmethod);
-	}
+	if (p->socket.type == SIP_TRANSPORT_UDP) {
+		int ack_res;
 
-	if (ack_res == FALSE) {
-		append_history(p, "Ignore", "Ignoring this retransmit\n");
-		return;
+		/* Acknowledge whatever it is destined for */
+		if ((resp >= 100) && (resp <= 199)) {
+			ack_res = __sip_semi_ack(p, seqno, 0, sipmethod);
+		} else {
+			ack_res = __sip_ack(p, seqno, 0, sipmethod);
+		}
+
+		if (ack_res == FALSE) {
+			append_history(p, "Ignore", "Ignoring this retransmit\n");
+			return;
+		}
 	}
 
 	/* If this is a NOTIFY for a subscription clear the flag that indicates that we have a NOTIFY pending */
