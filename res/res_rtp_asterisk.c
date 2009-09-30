7ef3a1a5e2b747426ba59132debdfe091419abf1
Automerge script <automerge@asterisk.org>
2009-09-30 18:23:24 +0000

    Merged revisions 221266,221278 via svnmerge from file:///srv/subversion/repos/asterisk/trunk
    ................
      r221266 | twilson | 2009-09-30 12:52:30 -0500 (Wed, 30 Sep 2009) | 32 lines
      
      Merged revisions 221086 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.4
      
      ........
        r221086 | twilson | 2009-09-30 09:49:11 -0500 (Wed, 30 Sep 2009) | 25 lines
        
        Change the SSRC by default when our media stream changes
        
        Be default, change SSRC when doing an audio stream changes Asterisk doesn't
        honor marker bit when reinvited to already-bridged RTP streams,resulting in
        far-end stack discarding packets with "old" timestamps that areactually part of
        a new stream.  This patch sends AST_CONTROL_SRCUPDATE whenever there is a
        reinvite, unless the 'constantssrc' is set to true in sip.conf.
        
        The original issue reported to Digium support detailed the following situation:
        ITSP <-> Asterisk 1.4.26.2 <-> SIP-based Application Server Call comes in
        fromITSP, Asterisk dials the app server which sends a re-invite back
        toAsterisk--not to negotiate to send media directly to the ITSP, but to
        indicatethat it's changing the stream it's sending to Asterisk.  The app
        servergenerates a new SSRC, sequence numbers, timestamps, and sets the marker
        bit on the new stream.  Asterisk passes through the teimstamp of the new stream,
        butdoes not reset the SSRC, sequence numbers, or set the marker bit.
        
        When the timestamp on the new stream is older than the timestamp on the
        originalstream, the ITSP (which doesn't know there has been any change) discards
        the newframes because it thinks they are too old.  This patch addresses this by
        changing the SSRC on a stream update unless constantssrc=true is set in
        sip.conf.
        
        Review: https://reviewboard.asterisk.org/r/374/
      ........
    ................
      r221278 | twilson | 2009-09-30 13:21:03 -0500 (Wed, 30 Sep 2009) | 4 lines
      
      Use rtp properties instead of adding a callback
      
      Thanks, Josh.
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/astum@221285 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_rtp_asterisk.c
+++ res/res_rtp_asterisk.c
@@ -660,6 +660,11 @@ static void ast_rtp_new_source(struct ast_rtp_instance *instance)
 	/* We simply set this bit so that the next packet sent will have the marker bit turned on */
 	ast_set_flag(rtp, FLAG_NEED_MARKER_BIT);
 
+	if (!ast_rtp_instance_get_prop(instance, AST_RTP_PROPERTY_CONSTANT_SSRC)) {
+		ast_log(LOG_ERROR, "Changing ssrc\n");
+		rtp->ssrc = ast_random();
+	}
+
 	return;
 }
 
