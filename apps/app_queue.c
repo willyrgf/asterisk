3ad00893c5e5bb938cf5916c1cfdd8367c61999f
Automerge script <automerge@asterisk.org>
2010-05-21 21:23:12 +0000

    Merged revisions 265089 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
      r265089 | mmichelson | 2010-05-21 15:59:14 -0500 (Fri, 21 May 2010) | 8 lines
      
      Don't hang up on a queue caller if the file we attempt to play does not exist.
      
      This also fixes a documentation mistake in file.h that made my original attempt
      to correct this problem not work correctly.
      
      (closes issue #17061)
      Reported by: RoadKill
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/cancel_elsewhere_1.4@265117 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_queue.c
+++ apps/app_queue.c
@@ -1535,6 +1535,10 @@ static int play_file(struct ast_channel *chan, char *filename)
 		return 0;
 	}
 
+	if (!ast_fileexists(filename, NULL, chan->language)) {
+		return 0;
+	}
+
 	ast_stopstream(chan);
 
 	res = ast_streamfile(chan, filename, chan->language);
