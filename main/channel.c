cdc7bbbeca604f1b5e5095ff9994ce93b5eb42c9
Automerge script <automerge@asterisk.org>
2010-09-20 23:26:14 +0000

    Merged revisions 287682,287684 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
      r287682 | alecdavis | 2010-09-20 17:57:48 -0500 (Mon, 20 Sep 2010) | 18 lines
      
      ast_channel_masquerade: Avoid recursive masquerades.
      
      Check all 4 combinations of (original/clonechan) * (masq/masqr).
      
      Initially original->masq and clonechan->masqr were only checked.
      
      It's possible with multiple masq's planned - and not yet executed, that
       the 'original' chan could already have another masq'd into it - thus original->masqr
      would be set, that masqr would lost.
      Likewise for the clonechan->masq.
      
      (closes issue #16057;#17363)
      Reported by: amorsen;davidw,alecdavis
      Patches: 
            bug16057.diff4.txt uploaded by alecdavis (license 585)
      Tested by: ramonpeek, davidw, alecdavis
    ........
      r287684 | alecdavis | 2010-09-20 18:15:25 -0500 (Mon, 20 Sep 2010) | 10 lines
      
      ast_channel_masquerade: remove extra else if
      
      (closes issue #17363,#16057)
      
      Reported by: amorsen;davidw,alecdavis
      Patches: 
            based on bug16057.diff4.txt uploaded by alecdavis (license 585)
      Tested by: ramonpeek, davidw, alecdavis
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/moremanager@287729 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/channel.c
+++ main/channel.c
@@ -3921,13 +3921,8 @@ retrymasq:
 	if (option_debug)
 		ast_log(LOG_DEBUG, "Planning to masquerade channel %s into the structure of %s\n",
 			clone->name, original->name);
-	if (original->masq) {
-		ast_log(LOG_WARNING, "%s is already going to masquerade as %s\n",
-			original->masq->name, original->name);
-	} else if (clone->masqr) {
-		ast_log(LOG_WARNING, "%s is already going to masquerade as %s\n",
-			clone->name, clone->masqr->name);
-	} else {
+
+	if (!original->masqr && !original->masq && !clone->masq && !clone->masqr) {
 		original->masq = clone;
 		clone->masqr = original;
 		ast_queue_frame(original, &ast_null_frame);
@@ -3935,6 +3930,20 @@ retrymasq:
 		if (option_debug)
 			ast_log(LOG_DEBUG, "Done planning to masquerade channel %s into the structure of %s\n", clone->name, original->name);
 		res = 0;
+
+	} else if (original->masq) {
+		ast_log(LOG_WARNING, "%s is already going to masquerade as %s\n",
+			original->masq->name, original->name);
+	} else if (original->masqr) {
+		/* not yet as a previously planned masq hasn't yet happened */
+		ast_log(LOG_WARNING, "%s is already going to masquerade as %s\n",
+			original->name, original->masqr->name);
+	} else if (clone->masq) {
+		ast_log(LOG_WARNING, "%s is already going to masquerade as %s\n",
+			clone->masq->name, clone->name);
+	} else { /* (clone->masqr) */
+		ast_log(LOG_WARNING, "%s is already going to masquerade as %s\n",
+			clone->name, clone->masqr->name);
 	}
 
 	ast_channel_unlock(clone);
