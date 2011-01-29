f35ec60ffb0560e20c168ec355410185d01be8e3
Automerge script <automerge@asterisk.org>
2011-01-29 22:22:24 +0000

    Merged revisions 304820 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
      r304820 | seanbright | 2011-01-29 15:48:30 -0600 (Sat, 29 Jan 2011) | 5 lines
      
      Backport MeetMe related reference leaks fixes from 1.6.2/1.8/trunk.
      
      I had forgotten that MeetMe in 1.4 also used astobj2, so backport the fixes
      where appropriate.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/meetmefixes-1.4@304842 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_meetme.c
+++ apps/app_meetme.c
@@ -833,6 +833,11 @@ static struct ast_conference *build_conf(char *confno, char *pin, char *pinadmin
 		ast_log(LOG_WARNING, "Unable to open DAHDI pseudo device\n");
 		if (cnf->fd >= 0)
 			close(cnf->fd);
+		ao2_ref(cnf->usercontainer, -1);
+		ast_mutex_destroy(&cnf->playlock);
+		ast_mutex_destroy(&cnf->listenlock);
+		ast_mutex_destroy(&cnf->recordthreadlock);
+		ast_mutex_destroy(&cnf->announcethreadlock);
 		free(cnf);
 		cnf = NULL;
 		goto cnfout;
@@ -854,6 +859,11 @@ static struct ast_conference *build_conf(char *confno, char *pin, char *pinadmin
 				ast_hangup(cnf->chan);
 			else
 				close(cnf->fd);
+			ao2_ref(cnf->usercontainer, -1);
+			ast_mutex_destroy(&cnf->playlock);
+			ast_mutex_destroy(&cnf->listenlock);
+			ast_mutex_destroy(&cnf->recordthreadlock);
+			ast_mutex_destroy(&cnf->announcethreadlock);
 			free(cnf);
 			cnf = NULL;
 			goto cnfout;
@@ -1575,7 +1585,6 @@ static void set_user_talking(struct ast_channel *chan, struct ast_conference *co
 static int conf_run(struct ast_channel *chan, struct ast_conference *conf, int confflags, char *optargs[])
 {
 	struct ast_conf_user *user = NULL;
-	struct ast_conf_user *usr = NULL;
 	int fd;
 	struct dahdi_confinfo ztc, ztc_empty;
 	struct ast_frame *f;
@@ -1865,7 +1874,7 @@ static int conf_run(struct ast_channel *chan, struct ast_conference *conf, int c
 	if (!(confflags & CONFFLAG_QUIET) && ((confflags & CONFFLAG_INTROUSER) || (confflags & CONFFLAG_INTROUSERNOREVIEW)) && conf->users > 1) {
 		struct announce_listitem *item;
 		if (!(item = ao2_alloc(sizeof(*item), NULL)))
-			return -1;
+			goto outrun;
 		ast_copy_string(item->namerecloc, user->namerecloc, sizeof(item->namerecloc));
 		ast_copy_string(item->language, chan->language, sizeof(item->language));
 		item->confchan = conf->chan;
@@ -2252,6 +2261,7 @@ static int conf_run(struct ast_channel *chan, struct ast_conference *conf, int c
 								break;
 							case '3': /* Eject last user */
 							{
+								struct ast_conf_user *usr = NULL;
 								int max_no = 0;
 								ao2_callback(conf->usercontainer, OBJ_NODATA, user_max_cmp, &max_no);
 								menu_active = 0;
@@ -2262,7 +2272,7 @@ static int conf_run(struct ast_channel *chan, struct ast_conference *conf, int c
 								} else {
 									usr->adminflags |= ADMINFLAG_KICKME;
 								}
-								ao2_ref(user, -1);
+								ao2_ref(usr, -1);
 								ast_stopstream(chan);
 								break;	
 							}
@@ -2504,7 +2514,7 @@ bailoutandtrynormal:
 	if (!(confflags & CONFFLAG_QUIET) && ((confflags & CONFFLAG_INTROUSER) || (confflags & CONFFLAG_INTROUSERNOREVIEW)) && conf->users > 1) {
 		struct announce_listitem *item;
 		if (!(item = ao2_alloc(sizeof(*item), NULL)))
-			return -1;
+			goto outrun;
 		ast_copy_string(item->namerecloc, user->namerecloc, sizeof(item->namerecloc));
 		ast_copy_string(item->language, chan->language, sizeof(item->language));
 		item->confchan = conf->chan;
@@ -2525,9 +2535,8 @@ bailoutandtrynormal:
 	if (dsp)
 		ast_dsp_free(dsp);
 	
-	if (!user->user_no) {
-		ao2_ref(user, -1);
-	} else { /* Only cleanup users who really joined! */
+	if (user->user_no) {
+		/* Only cleanup users who really joined! */
 		now = time(NULL);
 		hr = (now - user->jointime) / 3600;
 		min = ((now - user->jointime) % 3600) / 60;
@@ -2568,6 +2577,7 @@ bailoutandtrynormal:
 		snprintf(meetmesecs, sizeof(meetmesecs), "%d", (int) (time(NULL) - user->jointime));
 		pbx_builtin_setvar_helper(chan, "MEETMESECS", meetmesecs);
 	}
+	ao2_ref(user, -1);
 	AST_LIST_UNLOCK(&confs);
 
 	return ret;
