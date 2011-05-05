622fe4ba28f5310b60fd7e6ac1cd0434f6cfe240
Automerge script <automerge@asterisk.org>
2011-05-05 18:30:52 +0000

    Merged revisions 317211 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.4
    ........
      r317211 | russell | 2011-05-05 13:20:29 -0500 (Thu, 05 May 2011) | 15 lines
      
      chan_sip: fix broken realtime peer count, fix memory leak
      
      This patch addresses two bugs in chan_sip:
      
      1) The count of realtime peers and users was off.  The increment checked the
      value of the caching option, while the decrement did not.
      
      2) Add a missing regfree() for a regex.
      
      (closes issue #19108)
      Reported by: vrban
      Patches:
            missing_regfree.patch uploaded by vrban (license 756)
            sip_object_counter.patch uploaded by vrban (license 756)
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/sipchanstats@317260 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -2787,7 +2787,7 @@ static void sip_destroy_peer(struct sip_peer *peer)
 	ast_free_ha(peer->ha);
 	if (ast_test_flag(&peer->flags[1], SIP_PAGE2_SELFDESTRUCT))
 		apeerobjs--;
-	else if (ast_test_flag(&peer->flags[0], SIP_REALTIME))
+	else if (!ast_test_flag(&global_flags[1], SIP_PAGE2_RTCACHEFRIENDS) && ast_test_flag(&peer->flags[0], SIP_REALTIME))
 		rpeerobjs--;
 	else
 		speerobjs--;
@@ -2995,7 +2995,7 @@ static void sip_destroy_user(struct sip_user *user)
 		ast_variables_destroy(user->chanvars);
 		user->chanvars = NULL;
 	}
-	if (ast_test_flag(&user->flags[0], SIP_REALTIME))
+	if (!ast_test_flag(&global_flags[1], SIP_PAGE2_RTCACHEFRIENDS) && ast_test_flag(&user->flags[0], SIP_REALTIME))
 		ruserobjs--;
 	else
 		suserobjs--;
@@ -11494,6 +11494,7 @@ static int sip_prune_realtime(int fd, int argc, char *argv[])
 	int multi = FALSE;
 	char *name = NULL;
 	regex_t regexbuf;
+	int havepattern = 0;
 
 	switch (argc) {
 	case 4:
@@ -11552,8 +11553,10 @@ static int sip_prune_realtime(int fd, int argc, char *argv[])
 	}
 
 	if (multi && name) {
-		if (regcomp(&regexbuf, name, REG_EXTENDED | REG_NOSUB))
+		if (regcomp(&regexbuf, name, REG_EXTENDED | REG_NOSUB)) {
 			return RESULT_SHOWUSAGE;
+		}
+		havepattern = 1;
 	}
 
 	if (multi) {
@@ -11626,6 +11629,10 @@ static int sip_prune_realtime(int fd, int argc, char *argv[])
 			} else
 				ast_cli(fd, "User '%s' not found.\n", name);
 		}
+	}
+
+	if (havepattern) {
+		regfree(&regexbuf);
 	}
 
 	return RESULT_SUCCESS;
