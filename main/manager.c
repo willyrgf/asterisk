cbbfdf1b60ce2f3c8031e4e57ad14665f2328493
Automerge script <automerge@asterisk.org>
2011-05-23 18:25:55 +0000

    Merged revisions 320651 via svnmerge from file:///srv/subversion/repos/asterisk/trunk
    ................
      r320651 | rmudgett | 2011-05-23 13:00:02 -0500 (Mon, 23 May 2011) | 23 lines
      
      Merged revisions 320650 via svnmerge from 
      https://origsvn.digium.com/svn/asterisk/branches/1.8
      
      ........
        r320650 | rmudgett | 2011-05-23 12:53:44 -0500 (Mon, 23 May 2011) | 16 lines
        
        Add ConnectedLineNum/Name headers to output of AMI action Status.
        
        * Add ConnectedLineNum and ConnectedLineName headers to the output of the
        AMI action Status.  This makes it easier to find out who the channel is
        connected to without having to lookup BridgedChannel or when they are
        connected to an application (e.g.: VoiceMail) which has no bridged
        channel.
        
        * Bridged channels with no CallerID had "" instead of "<unknown>" output,
        that might be a bug as "<unknown>" was what older versions used.
        
        (closes issue #18158)
        Reported by: gareth
        Patches:
              svn-292308.diff uploaded by gareth (license 208)
      ........
    ................
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/manager-check-trunk@320684 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/manager.c
+++ main/manager.c
@@ -3204,6 +3204,8 @@ static int action_status(struct mansession *s, const struct message *m)
 			"Channel: %s\r\n"
 			"CallerIDNum: %s\r\n"
 			"CallerIDName: %s\r\n"
+			"ConnectedLineNum: %s\r\n"
+			"ConnectedLineName: %s\r\n"
 			"Accountcode: %s\r\n"
 			"ChannelState: %d\r\n"
 			"ChannelStateDesc: %s\r\n"
@@ -3217,8 +3219,10 @@ static int action_status(struct mansession *s, const struct message *m)
 			"%s"
 			"\r\n",
 			c->name,
-			S_COR(c->caller.id.number.valid, c->caller.id.number.str, ""),
-			S_COR(c->caller.id.name.valid, c->caller.id.name.str, ""),
+			S_COR(c->caller.id.number.valid, c->caller.id.number.str, "<unknown>"),
+			S_COR(c->caller.id.name.valid, c->caller.id.name.str, "<unknown>"),
+			S_COR(c->connected.id.number.valid, c->connected.id.number.str, "<unknown>"),
+			S_COR(c->connected.id.name.valid, c->connected.id.name.str, "<unknown>"),
 			c->accountcode,
 			c->_state,
 			ast_state2str(c->_state), c->context,
@@ -3230,6 +3234,8 @@ static int action_status(struct mansession *s, const struct message *m)
 				"Channel: %s\r\n"
 				"CallerIDNum: %s\r\n"
 				"CallerIDName: %s\r\n"
+				"ConnectedLineNum: %s\r\n"
+				"ConnectedLineName: %s\r\n"
 				"Account: %s\r\n"
 				"State: %s\r\n"
 				"%s"
@@ -3240,6 +3246,8 @@ static int action_status(struct mansession *s, const struct message *m)
 				c->name,
 				S_COR(c->caller.id.number.valid, c->caller.id.number.str, "<unknown>"),
 				S_COR(c->caller.id.name.valid, c->caller.id.name.str, "<unknown>"),
+				S_COR(c->connected.id.number.valid, c->connected.id.number.str, "<unknown>"),
+				S_COR(c->connected.id.name.valid, c->connected.id.name.str, "<unknown>"),
 				c->accountcode,
 				ast_state2str(c->_state), bridge, c->uniqueid,
 				ast_str_buffer(str), idText);
