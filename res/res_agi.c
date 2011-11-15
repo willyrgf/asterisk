f256cb09777defeae0dc02fb742346d60da01e95
Automerge script <automerge@asterisk.org>
2011-11-15 20:30:15 +0000

    Merged revisions 345431 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.8
    ........
      r345431 | rmudgett | 2011-11-15 14:09:02 -0600 (Tue, 15 Nov 2011) | 10 lines
      
      Make FastAGI HANGUP show up in AGI debug output.
      
      * Change from using send() to ast_agi_send() so the HANGUP shows up in the
      AGI debug output.
      
      (closes issue ASTERISK-18723)
      Reported by: James Van Vleet
      Patches:
            jira_asterisk_18723_v1.8.patch (license #5621) patch uploaded by rmudgett
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/res_agi_moh_streamfile@345473 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_agi.c
+++ res/res_agi.c
@@ -3488,7 +3488,7 @@ static enum agi_result run_agi(struct ast_channel *chan, char *request, AGI *agi
 				if (pid > -1) {
 					kill(pid, SIGHUP);
 				} else if (agi->fast) {
-					send(agi->ctrl, "HANGUP\n", 7, 0);
+					ast_agi_send(agi->fd, chan, "HANGUP\n");
 				}
 			}
 		}
@@ -3603,7 +3603,7 @@ static enum agi_result run_agi(struct ast_channel *chan, char *request, AGI *agi
 			}
 			waitpid(pid, status, WNOHANG);
 		} else if (agi->fast) {
-			send(agi->ctrl, "HANGUP\n", 7, 0);
+			ast_agi_send(agi->fd, chan, "HANGUP\n");
 		}
 	}
 	fclose(readf);
