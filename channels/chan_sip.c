ff36e462b779b3b8fc23c95cb616ddd83d3a8511
Automerge script <automerge@asterisk.org>
2012-09-25 00:20:33 +0000

    Merged revisions 373532 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.8
    ........
      r373532 | file | 2012-09-24 19:09:46 -0500 (Mon, 24 Sep 2012) | 2 lines
      
      Add missing checks that I neglected. The SIP technology and SIP info technology should be considered equal.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/rana-dtmf-duration-1.8@373544 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -29391,7 +29391,8 @@ static enum ast_rtp_glue_result sip_get_rtp_peer(struct ast_channel *chan, struc
 
 	if (!(opp_chan = ast_bridged_channel(chan))) {
 		return AST_RTP_GLUE_RESULT_FORBID;
-	} else if ((opp_chan->tech != &sip_tech) || (!(opp = opp_chan->tech_pvt))) {
+	} else if (((opp_chan->tech != &sip_tech) && (opp_chan->tech != &sip_tech_info)) ||
+		   (!(opp = opp_chan->tech_pvt))) {
 		return AST_RTP_GLUE_RESULT_FORBID;
 	}
 
@@ -29446,7 +29447,8 @@ static enum ast_rtp_glue_result sip_get_vrtp_peer(struct ast_channel *chan, stru
 
 	if (!(opp_chan = ast_bridged_channel(chan))) {
 		return AST_RTP_GLUE_RESULT_FORBID;
-	} else if ((opp_chan->tech != &sip_tech) || (!(opp = opp_chan->tech_pvt))) {
+	} else if (((opp_chan->tech != &sip_tech) && (opp_chan->tech != &sip_tech_info)) ||
+		   (!(opp = opp_chan->tech_pvt))) {
 		return AST_RTP_GLUE_RESULT_FORBID;
 	}
 
@@ -29492,7 +29494,8 @@ static enum ast_rtp_glue_result sip_get_trtp_peer(struct ast_channel *chan, stru
 
 	if (!(opp_chan = ast_bridged_channel(chan))) {
 		return AST_RTP_GLUE_RESULT_FORBID;
-	} else if ((opp_chan->tech != &sip_tech) || (!(opp = opp_chan->tech_pvt))) {
+	} else if (((opp_chan->tech != &sip_tech) && (opp_chan->tech != &sip_tech_info)) ||
+		   (!(opp = opp_chan->tech_pvt))) {
 		return AST_RTP_GLUE_RESULT_FORBID;
 	}
 
