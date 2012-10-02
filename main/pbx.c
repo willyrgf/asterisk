550b430f7eed660a95ccfa793aa6ba3ee3158e42
Automerge script <automerge@asterisk.org>
2012-10-02 01:19:36 +0000

    Merged revisions 374177 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.8
    ........
      r374177 | mjordan | 2012-10-01 19:31:14 -0500 (Mon, 01 Oct 2012) | 9 lines
      
      Fix a variety of ref counting issues
      
      This patch resolves a number of ref leaks that occur primarily on Asterisk
      shutdown.  It adds a variety of shutdown routines to core portions of
      Asterisk such that they can reclaim resources allocate duringd initialization.
      
      Review: https://reviewboard.asterisk.org/r/2137
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/bring-in-the-early-media-1.8@374185 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/pbx.c
+++ main/pbx.c
@@ -10315,6 +10315,26 @@ static const struct ast_data_entry pbx_data_providers[] = {
 	AST_DATA_ENTRY("asterisk/core/hints", &hints_data_provider),
 };
 
+/*! \internal \brief Clean up resources on Asterisk shutdown.
+ * \note Cleans up resources allocated in load_pbx */
+static void unload_pbx(void)
+{
+	int x;
+
+	if (device_state_sub) {
+		device_state_sub = ast_event_unsubscribe(device_state_sub);
+	}
+
+	/* Unregister builtin applications */
+	for (x = 0; x < ARRAY_LEN(builtins); x++) {
+		ast_unregister_application(builtins[x].name);
+	}
+	ast_manager_unregister("ShowDialPlan");
+	ast_custom_function_unregister(&exception_function);
+	ast_custom_function_unregister(&testtime_function);
+	ast_data_unregister(NULL);
+}
+
 int load_pbx(void)
 {
 	int x;
@@ -10348,6 +10368,7 @@ int load_pbx(void)
 		return -1;
 	}
 
+	ast_register_atexit(unload_pbx);
 	return 0;
 }
 
