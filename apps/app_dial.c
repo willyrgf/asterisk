b848d061892992ea71cb4a2e5de23b46fd46af10
Automerge script <automerge@asterisk.org>
2012-11-07 17:20:35 +0000

    Merged revisions 375993-375994 via svnmerge from file:///srv/subversion/repos/asterisk/branches/1.8
    ........
      r375993 | mmichelson | 2012-11-07 11:01:13 -0600 (Wed, 07 Nov 2012) | 30 lines
      
      Fix misuses of timeouts throughout the code.
      
      Prior to this change, a common method for determining if a timeout
      was reached was to call a function such as ast_waitfor_n() and inspect
      the out parameter that told how many milliseconds were left, then use
      that as the input to ast_waitfor_n() on the next go-around.
      
      The problem with this is that in some cases, submillisecond timeouts
      can occur, resulting in the out parameter not decreasing any. When this
      happens thousands of times, the result is that the timeout takes much
      longer than intended to be reached. As an example, I had a situation where
      a 3 second timeout took multiple days to finally end since most wakeups
      from ast_waitfor_n() were under a millisecond.
      
      This patch seeks to fix this pattern throughout the code. Now we log the
      time when an operation began and find the difference in wall clock time
      between now and when the event started. This means that sub-millisecond timeouts
      now cannot play havoc when trying to determine if something has timed out.
      
      Part of this fix also includes changing the function ast_waitfor() so that it
      is possible for it to return less than zero when a negative timeout is given
      to it. This makes it actually possible to detect errors in ast_waitfor() when
      there is no timeout.
      
      (closes issue ASTERISK-20414)
      reported by David M. Lee
      
      Review: https://reviewboard.asterisk.org/r/2135/
    ........
      r375994 | mmichelson | 2012-11-07 11:08:44 -0600 (Wed, 07 Nov 2012) | 3 lines
      
      Remove some debugging that accidentally made it in the last commit.
    ........
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/bring-in-the-early-media-1.8@376002 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- apps/app_dial.c
+++ apps/app_dial.c
@@ -1075,6 +1075,7 @@ static struct ast_channel *wait_for_answer(struct ast_channel *in,
 	int is_cc_recall;
 	int cc_frame_received = 0;
 	int num_ringing = 0;
+	struct timeval start = ast_tvnow();
 
 	ast_party_connected_line_init(&connected_caller);
 	if (single) {
@@ -1115,7 +1116,7 @@ static struct ast_channel *wait_for_answer(struct ast_channel *in,
 		ast_poll_channel_add(in, epollo->chan);
 #endif
 
-	while (*to && !peer) {
+	while ((*to = ast_remaining_ms(start, orig)) && !peer) {
 		struct chanlist *o;
 		int pos = 0; /* how many channels do we handle */
 		int numlines = prestart;
@@ -1648,10 +1649,13 @@ static struct ast_channel *wait_for_answer(struct ast_channel *in,
 skip_frame:;
 			ast_frfree(f);
 		}
-		if (!*to)
-			ast_verb(3, "Nobody picked up in %d ms\n", orig);
-		if (!*to || ast_check_hangup(in))
-			ast_cdr_noanswer(in->cdr);
+	}
+
+	if (!*to) {
+		ast_verb(3, "Nobody picked up in %d ms\n", orig);
+	}
+	if (!*to || ast_check_hangup(in)) {
+		ast_cdr_noanswer(in->cdr);
 	}
 
 #ifdef HAVE_EPOLL
