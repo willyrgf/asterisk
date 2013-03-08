502f4f8d834974b11bffb968009daed5e1183520
Olle Johansson <oej@edvina.net>
2013-03-08 11:02:33 +0000

    Why oh why do you stop all the RTP flows because you are simply on hold? RTP keepalives and RTCP still needs to flow, you little Asterisk rascal. Let's try this little surgical change and see what happens.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/pinefrog-rtcp-1.8@382681 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -9884,11 +9884,12 @@ static int process_sdp(struct sip_pvt *p, struct sip_request *req, int t38action
 		ast_queue_control_data(p->owner, AST_CONTROL_HOLD,
 				       S_OR(p->mohsuggest, NULL),
 				       !ast_strlen_zero(p->mohsuggest) ? strlen(p->mohsuggest) + 1 : 0);
-		if (sendonly) {
-			ast_rtp_instance_stop(p->rtp);
-			/* RTCP needs to go ahead, even if we're on hold!!! */
-			/* OEJ this is a BUG to fix. Now. */
-		}
+		//
+		// We should only stop at port = 0 or BYE /End of call or stream (like a transfer)
+		//if (sendonly == 2) {
+			//ast_rtp_instance_stop(p->rtp);
+			///* RTCP needs to go ahead, even if we're on hold!!! */
+		//}
 		/* Activate a re-invite */
 		ast_queue_frame(p->owner, &ast_null_frame);
 		change_hold_state(p, req, TRUE, sendonly);
