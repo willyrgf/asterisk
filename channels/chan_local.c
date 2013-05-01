51bf25d0c099f9189fd5549501b52a9d8324b92b
Automerge script <automerge@asterisk.org>
2013-05-01 21:25:02 +0000

    Multiple revisions 387209-387212,387220
    ........
      r387209 | rmudgett | 2013-05-01 15:35:25 -0500 (Wed, 01 May 2013) | 5 lines
      
      Make mod_load_cmp() not as klunky.
      
      There is a reason the heap comparison functions like qsort(), and other
      comparison functions specify <0, >0, and =0 for the return values.
    ........
      r387210 | rmudgett | 2013-05-01 15:53:30 -0500 (Wed, 01 May 2013) | 1 line
      
      Whitespace changes.
    ........
      r387211 | rmudgett | 2013-05-01 15:59:29 -0500 (Wed, 01 May 2013) | 5 lines
      
      Make chan_local locals container an explicit list container.
      
      Pretending that chan_local locals container can have more than one bucket
      is silly.  The container has no key to help search.
    ........
      r387212 | rmudgett | 2013-05-01 16:09:14 -0500 (Wed, 01 May 2013) | 1 line
      
      Trivial changes. Comments, parentheses, spelling, wording.
    ........
      r387220 | mjordan | 2013-05-01 16:18:24 -0500 (Wed, 01 May 2013) | 20 lines
      
      Clear the DTMF sending digit tracking on off nominal paths
      
      In certain situations, when the RTP engine goes to send a DTMF end digit
      it may be in a situation where the remote address is no longer available,
      or the digit that was supposed to be sent is invalid. In such cases, we
      need to clear the RTP counters appropriately. Otherwise, when the RTP
      source is set again, we'll continue to think that we're in the middle of
      sending a DTMF digit, which can confuse the remote party (signficantly).
      
      (closes issue ASTERISK-21522)
      Reported by: Corey Farrell
      patches:
        rtp_dtmf_process_end.patch uploaded by Corey Farrell (License 5909)
      ........
      
      Merged revisions 387213 from http://svn.asterisk.org/svn/asterisk/branches/1.8
      ........
      
      Merged revisions 387216 from http://svn.asterisk.org/svn/asterisk/branches/11
    ........
    
    Merged revisions 387209-387212,387220 from file:///srv/subversion/repos/asterisk/trunk
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/pinana-initialize-devstate-trunk@387245 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_local.c
+++ channels/chan_local.c
@@ -21,7 +21,7 @@
  * \author Mark Spencer <markster@digium.com>
  *
  * \brief Local Proxy Channel
- * 
+ *
  * \ingroup channel_drivers
  */
 
@@ -78,11 +78,6 @@ static const char tdesc[] = "Local Proxy Channel Driver";
 
 #define IS_OUTBOUND(a,b) (a == b->chan ? 1 : 0)
 
-/* right now we are treating the locals astobj2 container as a
- * list.  If there is ever a reason to make this more efficient
- * increasing the bucket size would help. */
-static const int BUCKET_SIZE = 1;
-
 static struct ao2_container *locals;
 
 static unsigned int name_sequence = 0;
@@ -136,21 +131,21 @@ static struct ast_channel_tech local_tech = {
 	.setoption = local_setoption,
 };
 
-/*! \brief the local pvt structure for all channels
-
-	The local channel pvt has two ast_chan objects - the "owner" and the "next channel", the outbound channel
-
-	ast_chan owner -> local_pvt -> ast_chan chan -> yet-another-pvt-depending-on-channel-type
-
-*/
+/*!
+ * \brief the local pvt structure for all channels
+ *
+ * The local channel pvt has two ast_chan objects - the "owner" and the "next channel", the outbound channel
+ *
+ * ast_chan owner -> local_pvt -> ast_chan chan -> yet-another-pvt-depending-on-channel-type
+ */
 struct local_pvt {
+	struct ast_channel *owner;      /*!< Master Channel - Bridging happens here */
+	struct ast_channel *chan;       /*!< Outbound channel - PBX is run here */
+	struct ast_format_cap *reqcap;  /*!< Requested format capabilities */
+	struct ast_jb_conf jb_conf;     /*!< jitterbuffer configuration for this local channel */
 	unsigned int flags;             /*!< Private flags */
 	char context[AST_MAX_CONTEXT];  /*!< Context to call */
 	char exten[AST_MAX_EXTENSION];  /*!< Extension to call */
-	struct ast_format_cap *reqcap;  /*!< Requested format capabilities */
-	struct ast_jb_conf jb_conf;     /*!< jitterbuffer configuration for this local channel */
-	struct ast_channel *owner;      /*!< Master Channel - Bridging happens here */
-	struct ast_channel *chan;       /*!< Outbound channel - PBX is run here */
 };
 
 #define LOCAL_ALREADY_MASQED  (1 << 0) /*!< Already masqueraded */
@@ -218,7 +213,7 @@ static void awesome_locking(struct local_pvt *p, struct ast_channel **outchan, s
 }
 
 /* Called with ast locked */
-static int local_setoption(struct ast_channel *ast, int option, void * data, int datalen)
+static int local_setoption(struct ast_channel *ast, int option, void *data, int datalen)
 {
 	int res = 0;
 	struct local_pvt *p;
@@ -296,7 +291,7 @@ static int local_devicestate(const char *data)
 	if (!context) {
 		ast_log(LOG_WARNING,
 			"Someone used Local/%s somewhere without a @context. This is bad.\n", data);
-		return AST_DEVICE_INVALID;	
+		return AST_DEVICE_INVALID;
 	}
 	*context++ = '\0';
 
@@ -409,7 +404,8 @@ query_cleanup:
 	return res;
 }
 
-/*! \brief queue a frame on a to either the p->owner or p->chan
+/*!
+ * \brief queue a frame onto either the p->owner or p->chan
  *
  * \note the local_pvt MUST have it's ref count bumped before entering this function and
  * decremented after this function is called.  This is a side effect of the deadlock
@@ -424,7 +420,6 @@ static int local_queue_frame(struct local_pvt *p, int isoutbound, struct ast_fra
 
 	/* Recalculate outbound channel */
 	other = isoutbound ? p->owner : p->chan;
-
 	if (!other) {
 		return 0;
 	}
@@ -472,6 +467,7 @@ static int local_answer(struct ast_channel *ast)
 	if (isoutbound) {
 		/* Pass along answer since somebody answered us */
 		struct ast_frame answer = { AST_FRAME_CONTROL, { AST_CONTROL_ANSWER } };
+
 		res = local_queue_frame(p, isoutbound, &answer, ast, 1);
 	} else {
 		ast_log(LOG_WARNING, "Huh?  Local is being asked to answer?\n");
@@ -685,7 +681,9 @@ static int local_fixup(struct ast_channel *oldchan, struct ast_channel *newchan)
 	}
 
 	/* Do not let a masquerade cause a Local channel to be bridged to itself! */
-	if (!ast_check_hangup(newchan) && ((p->owner && ast_channel_internal_bridged_channel(p->owner) == p->chan) || (p->chan && ast_channel_internal_bridged_channel(p->chan) == p->owner))) {
+	if (!ast_check_hangup(newchan)
+		&& ((p->owner && ast_channel_internal_bridged_channel(p->owner) == p->chan)
+			|| (p->chan && ast_channel_internal_bridged_channel(p->chan) == p->owner))) {
 		ast_log(LOG_WARNING, "You can not bridge a Local channel to itself!\n");
 		ao2_unlock(p);
 		ast_queue_hangup(newchan);
@@ -717,6 +715,7 @@ static int local_indicate(struct ast_channel *ast, int condition, const void *da
 	} else if (condition == AST_CONTROL_CONNECTED_LINE || condition == AST_CONTROL_REDIRECTING) {
 		struct ast_channel *this_channel;
 		struct ast_channel *the_other_channel;
+
 		/* A connected line update frame may only contain a partial amount of data, such
 		 * as just a source, or just a ton, and not the full amount of information. However,
 		 * the collected information is all stored in the outgoing channel's connectedline
@@ -735,6 +734,7 @@ static int local_indicate(struct ast_channel *ast, int condition, const void *da
 		}
 		if (the_other_channel) {
 			unsigned char frame_data[1024];
+
 			if (condition == AST_CONTROL_CONNECTED_LINE) {
 				if (isoutbound) {
 					ast_connected_line_copy_to_caller(ast_channel_caller(the_other_channel), ast_channel_connected(this_channel));
@@ -763,10 +763,10 @@ static int local_indicate(struct ast_channel *ast, int condition, const void *da
 			f.datalen = datalen;
 			res = local_queue_frame(p, isoutbound, &f, ast, 1);
 
-			if (!res && (condition == AST_CONTROL_T38_PARAMETERS) &&
-			    (datalen == sizeof(struct ast_control_t38_parameters))) {
+			if (!res && condition == AST_CONTROL_T38_PARAMETERS
+				&& datalen == sizeof(struct ast_control_t38_parameters)) {
 				const struct ast_control_t38_parameters *parameters = data;
-				
+
 				if (parameters->request_response == AST_T38_REQUEST_PARMS) {
 					res = AST_T38_REQUEST_PARMS;
 				}
@@ -967,7 +967,7 @@ static int local_call(struct ast_channel *ast, const char *dest, int timeout)
 		S_COR(ast_channel_caller(owner)->id.number.valid, ast_channel_caller(owner)->id.number.str, NULL))) {
 		ast_log(LOG_NOTICE, "No such extension/context %s@%s while calling Local channel\n", exten, context);
 		res = -1;
-		chan = ast_channel_unref(chan); /* we already unlocked it, so clear it hear so the cleanup label won't touch it. */
+		chan = ast_channel_unref(chan); /* we already unlocked it, so clear it here so the cleanup label won't touch it. */
 		goto return_cleanup;
 	}
 
@@ -997,20 +997,22 @@ static int local_call(struct ast_channel *ast, const char *dest, int timeout)
 		</managerEventInstance>
 	***/
 	manager_event(EVENT_FLAG_CALL, "LocalBridge",
-		      "Channel1: %s\r\n"
-		      "Channel2: %s\r\n"
-		      "Uniqueid1: %s\r\n"
-		      "Uniqueid2: %s\r\n"
-		      "Context: %s\r\n"
-		      "Exten: %s\r\n"
-		      "LocalOptimization: %s\r\n",
-			ast_channel_name(p->owner), ast_channel_name(p->chan), ast_channel_uniqueid(p->owner), ast_channel_uniqueid(p->chan),
-			p->context, p->exten,
-			ast_test_flag(p, LOCAL_NO_OPTIMIZATION) ? "Yes" : "No");
+		"Channel1: %s\r\n"
+		"Channel2: %s\r\n"
+		"Uniqueid1: %s\r\n"
+		"Uniqueid2: %s\r\n"
+		"Context: %s\r\n"
+		"Exten: %s\r\n"
+		"LocalOptimization: %s\r\n",
+		ast_channel_name(p->owner), ast_channel_name(p->chan),
+		ast_channel_uniqueid(p->owner), ast_channel_uniqueid(p->chan),
+		p->context, p->exten,
+		ast_test_flag(p, LOCAL_NO_OPTIMIZATION) ? "Yes" : "No");
 
 
 	/* Start switch on sub channel */
-	if (!(res = ast_pbx_start(chan))) {
+	res = ast_pbx_start(chan);
+	if (!res) {
 		ao2_lock(p);
 		ast_set_flag(p, LOCAL_LAUNCHED_PBX);
 		ao2_unlock(p);
@@ -1103,7 +1105,6 @@ static int local_hangup(struct ast_channel *ast)
 	if (!p->owner && !p->chan) {
 		ao2_unlock(p);
 
-		/* Remove from list */
 		ao2_unlink(locals, p);
 		ao2_ref(p, -1);
 		p = NULL;
@@ -1188,8 +1189,7 @@ static struct local_pvt *local_alloc(const char *data, struct ast_format_cap *ca
 			if (ast_test_flag(tmp, LOCAL_NO_OPTIMIZATION))
 				ast_set_flag(&tmp->jb_conf, AST_JB_ENABLED);
 			else {
-				ast_log(LOG_ERROR, "You must use the 'n' option for chan_local "
-					"to use the 'j' option to enable the jitterbuffer\n");
+				ast_log(LOG_ERROR, "You must use the 'n' option with the 'j' option to enable the jitter buffer\n");
 			}
 		}
 		if (strchr(opts, 'b')) {
@@ -1208,7 +1208,6 @@ static struct local_pvt *local_alloc(const char *data, struct ast_format_cap *ca
 	ast_copy_string(tmp->context, c ? c : "default", sizeof(tmp->context));
 	ast_copy_string(tmp->exten, parse, sizeof(tmp->exten));
 
-	/* Add to list */
 	ao2_link(locals, tmp);
 
 	return tmp; /* this is returned with a ref */
@@ -1372,7 +1371,6 @@ static int manager_optimize_away(struct mansession *s, const struct message *m)
 	struct ao2_iterator it;
 
 	channel = astman_get_header(m, "Channel");
-
 	if (ast_strlen_zero(channel)) {
 		astman_send_error(s, m, "'Channel' not specified.");
 		return 0;
@@ -1425,7 +1423,8 @@ static int load_module(void)
 	}
 	ast_format_cap_add_all(local_tech.capabilities);
 
-	if (!(locals = ao2_container_alloc(BUCKET_SIZE, NULL, locals_cmp_cb))) {
+	locals = ao2_container_alloc_list(AO2_ALLOC_OPT_LOCK_MUTEX, 0, NULL, locals_cmp_cb);
+	if (!locals) {
 		ast_format_cap_destroy(local_tech.capabilities);
 		return AST_MODULE_LOAD_FAILURE;
 	}
