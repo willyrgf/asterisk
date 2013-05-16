2dca89971cca26d177ea5c2a1795ad1839fc2acc
Olle Johansson <oej@edvina.net>
2013-05-16 08:45:52 +0000

    Add missing config
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/roibos-cng-support-1.8@388922 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -27690,6 +27690,7 @@ static int handle_common_options(struct ast_flags *flags, struct ast_flags *mask
 		ast_set_flag(&mask[1], SIP_PAGE2_BUGGY_MWI);
 		ast_set2_flag(&flags[1], ast_true(v->value), SIP_PAGE2_BUGGY_MWI);
 	} else if (!strcasecmp(v->name, "comfort-noise")) {
+		ast_set_flag(&mask[1], SIP_PAGE2_ALLOW_CN);
 		ast_set2_flag(&flags[1], ast_true(v->value), SIP_PAGE2_ALLOW_CN);
 	} else
 		res = 0;
