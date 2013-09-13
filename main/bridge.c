74b3e372ffba3f9660020ff80f930bc3884e7c58
Automerge script <automerge@asterisk.org>
2013-09-13 00:20:14 +0000

    Multiple revisions 399018,399020,399022
    ........
      r399018 | newtonr | 2013-09-12 18:23:12 -0500 (Thu, 12 Sep 2013) | 17 lines
      
      Documentation fix and improvements to XML configuration help res_pjsip_acl
      
       *  One bug fix. Made the synopsis for "type" to accurate.
       *  changing the usage of "IP-domains" to "IP addresses"
       *  clarifying the usage for the options, by adding a relevant description for
          each
       *  modified other areas of the XML help for clarity, such as the module
          description and a few synopsis changes here and there. See the patch.
      
      (issue ASTERISK-22458)
      (closes issue ASTERISK-22458)
      Reported By: Rusty Newton
      Review: https://reviewboard.asterisk.org/r/2823/
      ........
      
      Merged revisions 399017 from http://svn.asterisk.org/svn/asterisk/branches/12
    ........
      r399020 | rmudgett | 2013-09-12 18:36:33 -0500 (Thu, 12 Sep 2013) | 5 lines
      
      astobj2: Register the bridges container for debug inspection.
      ........
      
      Merged revisions 399019 from http://svn.asterisk.org/svn/asterisk/branches/12
    ........
      r399022 | rmudgett | 2013-09-12 18:42:23 -0500 (Thu, 12 Sep 2013) | 8 lines
      
      CLI bridge: Fix "bridge destroy <id>" and "bridge kick <id> <chan>" tab completion.
      
      These two commands must deal with the live bridges container for tab
      completion and not the stasis cache.
      ........
      
      Merged revisions 399021 from http://svn.asterisk.org/svn/asterisk/branches/12
    ........
    
    Merged revisions 399018,399020,399022 from file:///srv/subversion/repos/asterisk/trunk
    ........
    
    Merged revisions 399028 from file:///srv/subversion/repos/asterisk/team/rmudgett/bridge_phase
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/rmudgett/bridge_tasks@399029 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/bridge.c
+++ main/bridge.c
@@ -4501,7 +4501,42 @@ static int bridge_sort_cmp(const void *obj_left, const void *obj_right, int flag
 	return cmp;
 }
 
-static char *complete_bridge(const char *word, int state)
+struct bridge_complete {
+	/*! Nth match to return. */
+	int state;
+	/*! Which match currently on. */
+	int which;
+};
+
+static int complete_bridge_live_search(void *obj, void *arg, void *data, int flags)
+{
+	struct bridge_complete *search = data;
+
+	if (++search->which > search->state) {
+		return CMP_MATCH;
+	}
+	return 0;
+}
+
+static char *complete_bridge_live(const char *word, int state)
+{
+	char *ret;
+	struct ast_bridge *bridge;
+	struct bridge_complete search = {
+		.state = state,
+		};
+
+	bridge = ao2_callback_data(bridges, ast_strlen_zero(word) ? 0 : OBJ_PARTIAL_KEY,
+		complete_bridge_live_search, (char *) word, &search);
+	if (!bridge) {
+		return NULL;
+	}
+	ret = ast_strdup(bridge->uniqueid);
+	ao2_ref(bridge, -1);
+	return ret;
+}
+
+static char *complete_bridge_stasis(const char *word, int state)
 {
 	char *ret = NULL;
 	int wordlen = strlen(word), which = 0;
@@ -4509,7 +4544,8 @@ static char *complete_bridge(const char *word, int state)
 	struct ao2_iterator iter;
 	struct stasis_message *msg;
 
-	if (!(cached_bridges = stasis_cache_dump(ast_bridge_cache(), ast_bridge_snapshot_type()))) {
+	cached_bridges = stasis_cache_dump(ast_bridge_cache(), ast_bridge_snapshot_type());
+	if (!cached_bridges) {
 		return NULL;
 	}
 
@@ -4547,7 +4583,8 @@ static char *handle_bridge_show_all(struct ast_cli_entry *e, int cmd, struct ast
 		return NULL;
 	}
 
-	if (!(cached_bridges = stasis_cache_dump(ast_bridge_cache(), ast_bridge_snapshot_type()))) {
+	cached_bridges = stasis_cache_dump(ast_bridge_cache(), ast_bridge_snapshot_type());
+	if (!cached_bridges) {
 		ast_cli(a->fd, "Failed to retrieve cached bridges\n");
 		return CLI_SUCCESS;
 	}
@@ -4579,7 +4616,8 @@ static int bridge_show_specific_print_channel(void *obj, void *arg, int flags)
 	RAII_VAR(struct stasis_message *, msg, NULL, ao2_cleanup);
 	struct ast_channel_snapshot *snapshot;
 
-	if (!(msg = stasis_cache_get(ast_channel_cache(), ast_channel_snapshot_type(), uniqueid))) {
+	msg = stasis_cache_get(ast_channel_cache(), ast_channel_snapshot_type(), uniqueid);
+	if (!msg) {
 		return 0;
 	}
 	snapshot = stasis_message_data(msg);
@@ -4603,7 +4641,7 @@ static char *handle_bridge_show_specific(struct ast_cli_entry *e, int cmd, struc
 		return NULL;
 	case CLI_GENERATE:
 		if (a->pos == 2) {
-			return complete_bridge(a->word, a->n);
+			return complete_bridge_stasis(a->word, a->n);
 		}
 		return NULL;
 	}
@@ -4641,7 +4679,7 @@ static char *handle_bridge_destroy_specific(struct ast_cli_entry *e, int cmd, st
 		return NULL;
 	case CLI_GENERATE:
 		if (a->pos == 2) {
-			return complete_bridge(a->word, a->n);
+			return complete_bridge_live(a->word, a->n);
 		}
 		return NULL;
 	}
@@ -4704,7 +4742,7 @@ static char *handle_bridge_kick_channel(struct ast_cli_entry *e, int cmd, struct
 		return NULL;
 	case CLI_GENERATE:
 		if (a->pos == 2) {
-			return complete_bridge(a->word, a->n);
+			return complete_bridge_live(a->word, a->n);
 		}
 		if (a->pos == 3) {
 			return complete_bridge_participant(a->argv[2], a->line, a->word, a->pos, a->n);
@@ -4877,6 +4915,28 @@ static struct ast_cli_entry bridge_cli[] = {
 
 /*!
  * \internal
+ * \brief Print bridge object key (name).
+ * \since 12.0.0
+ *
+ * \param v_obj A pointer to the object we want the key printed.
+ * \param where User data needed by prnt to determine where to put output.
+ * \param prnt Print output callback function to use.
+ *
+ * \return Nothing
+ */
+static void bridge_prnt_obj(void *v_obj, void *where, ao2_prnt_fn *prnt)
+{
+	struct ast_bridge *bridge = v_obj;
+
+	if (!bridge) {
+		return;
+	}
+	prnt(where, "%s %s chans:%d",
+		bridge->uniqueid, bridge->v_table->name, bridge->num_channels);
+}
+
+/*!
+ * \internal
  * \brief Shutdown the bridging system.
  * \since 12.0.0
  *
@@ -4885,6 +4945,7 @@ static struct ast_cli_entry bridge_cli[] = {
 static void bridge_shutdown(void)
 {
 	ast_cli_unregister_multiple(bridge_cli, ARRAY_LEN(bridge_cli));
+	ao2_container_unregister("bridges");
 	ao2_cleanup(bridges);
 	bridges = NULL;
 	ao2_cleanup(bridge_manager);
@@ -4909,6 +4970,7 @@ int ast_bridging_init(void)
 	if (!bridges) {
 		return -1;
 	}
+	ao2_container_register("bridges", bridges, bridge_prnt_obj);
 
 	ast_bridging_init_basic();
 
