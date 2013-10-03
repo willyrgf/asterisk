bf3e68c8aefdf3ef3608cb6842475d8961cd0e56
Automerge script <automerge@asterisk.org>
2013-10-03 15:21:01 +0000

    Multiple revisions 400361,400363-400364
    ........
      r400361 | file | 2013-10-03 09:52:24 -0500 (Thu, 03 Oct 2013) | 11 lines
      
      Fix crashes in res_pjsip_sdp_rtp and res_pjsip_t38 when a stream is rejected and external_media_address is set.
      
      The callback function for changing the media address in streams wrongly assumes that a connection line
      will always be present. This is false as no line is present if a stream has been rejected.
      
      (closes issue ASTERISK-22645)
      Reported by: Rusty Newton
      ........
      
      Merged revisions 400360 from http://svn.asterisk.org/svn/asterisk/branches/12
    ........
      r400363 | mmichelson | 2013-10-03 09:58:16 -0500 (Thu, 03 Oct 2013) | 17 lines
      
      Cache string values of formats on ast_format_cap() to save processing.
      
      Channel snapshots have string representations of the channel's native formats.
      Prior to this change, the format strings were re-created on ever channel snapshot
      creation. Since channel native formats rarely change, this was very wasteful.
      Now, string representations of formats may optionally be stored on the ast_format_cap
      for cases where string representations may be requested frequently. When formats
      are altered, the string cache is marked as invalid. When strings are requested, the
      cache validity is checked. If the cache is valid, then the cached strings are copied.
      If the cache is invalid, then the string cache is rebuilt and copied, and the cache
      is marked as being valid again.
      
      Review: https://reviewboard.asterisk.org/r/2879
      ........
      
      Merged revisions 400356 from http://svn.asterisk.org/svn/asterisk/branches/12
    ........
      r400364 | mmichelson | 2013-10-03 09:59:29 -0500 (Thu, 03 Oct 2013) | 5 lines
      
      Get rid of uses of stasis_topic_wait()
      ........
      
      Merged revisions 400362 from http://svn.asterisk.org/svn/asterisk/branches/12
    ........
    
    Merged revisions 400361,400363-400364 from file:///srv/subversion/repos/asterisk/trunk
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/rmudgett/ao2_enhancements@400368 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_pjsip_sdp_rtp.c
+++ res/res_pjsip_sdp_rtp.c
@@ -205,8 +205,8 @@ static int set_caps(struct ast_sip_session *session, struct ast_sip_session_medi
 	int direct_media_enabled = !ast_sockaddr_isnull(&session_media->direct_media_addr) &&
 		!ast_format_cap_is_empty(session->direct_media_cap);
 
-	if (!(caps = ast_format_cap_alloc_nolock()) ||
-	    !(peer = ast_format_cap_alloc_nolock())) {
+	if (!(caps = ast_format_cap_alloc(AST_FORMAT_CAP_FLAG_NOLOCK)) ||
+	    !(peer = ast_format_cap_alloc(AST_FORMAT_CAP_FLAG_NOLOCK))) {
 		ast_log(LOG_ERROR, "Failed to allocate %s capabilities\n", session_media->stream_type);
 		return -1;
 	}
@@ -910,7 +910,7 @@ static int create_outgoing_sdp_stream(struct ast_sip_session *session, struct as
 	/* Add ICE attributes and candidates */
 	add_ice_to_stream(session, session_media, pool, media);
 
-	if (!(caps = ast_format_cap_alloc_nolock())) {
+	if (!(caps = ast_format_cap_alloc(AST_FORMAT_CAP_FLAG_NOLOCK))) {
 		ast_log(LOG_ERROR, "Failed to allocate %s capabilities\n", session_media->stream_type);
 		return -1;
 	}
@@ -1096,6 +1096,11 @@ static void change_outgoing_sdp_stream_media_address(pjsip_tx_data *tdata, struc
 {
 	char host[NI_MAXHOST];
 	struct ast_sockaddr addr = { { 0, } };
+
+	/* If the stream has been rejected there will be no connection line */
+	if (!stream->conn) {
+		return;
+	}
 
 	ast_copy_pj_str(host, &stream->conn->addr, sizeof(host));
 	ast_sockaddr_parse(&addr, host, PARSE_PORT_FORBID);
