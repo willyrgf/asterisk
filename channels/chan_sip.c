78eae27fe7d6ef8a2e0f9298c67634803ba06f70
Automerge script <automerge@asterisk.org>
2014-03-27 19:34:23 +0000

    Fix dialplan function NULL channel safety issues
    (closes issue ASTERISK-23391)
    Reported by: Corey Farrell
    Review: https://reviewboard.asterisk.org/r/3386/
    ........
    
    Merged revisions 411313 from file:///srv/subversion/repos/asterisk/branches/1.8
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/oej/set-sip-tonezone-1.8@411358 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- channels/chan_sip.c
+++ channels/chan_sip.c
@@ -20134,7 +20134,12 @@ static int func_header_read(struct ast_channel *chan, const char *function, char
 	);
 	int i, number, start = 0;
 
- 	if (ast_strlen_zero(data)) {
+	if (!chan) {
+		ast_log(LOG_WARNING, "No channel was provided to %s function.\n", function);
+		return -1;
+	}
+
+	if (ast_strlen_zero(data)) {
 		ast_log(LOG_WARNING, "This function requires a header name.\n");
 		return -1;
 	}
@@ -20307,8 +20312,13 @@ static int function_sipchaninfo_read(struct ast_channel *chan, const char *cmd, 
 	static int deprecated = 0;
 
 	*buf = 0;
-	
- 	if (!data) {
+
+	if (!chan) {
+		ast_log(LOG_WARNING, "No channel was provided to %s function.\n", cmd);
+		return -1;
+	}
+
+	if (!data) {
 		ast_log(LOG_WARNING, "This function requires a parameter name.\n");
 		return -1;
 	}
