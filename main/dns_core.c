21e89ddceaf6edd28dafb034ff074bffd1f25d38
Mark Michelson <mmichelson@digium.com>
2015-03-05 16:46:57 +0000

    Fix bug where a NULL canonical name in a DNS result would cause a crash.
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/group/dns@432487 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- main/dns_core.c
+++ main/dns_core.c
@@ -368,6 +368,12 @@ int ast_dns_resolver_set_result(struct ast_dns_query *query, unsigned int nxdoma
 		return -1;
 	}
 
+	if (ast_strlen_zero(canonical)) {
+		ast_debug(2, "Query '%p': Could not set result information since no canonical name was provided\n",
+			query);
+		return -1;
+	}
+
 	if (query->result) {
 		ast_dns_result_free(query->result);
 	}
