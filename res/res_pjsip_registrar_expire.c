a409d01e4ca9da5bf198c5698ba3ce17c169fdce
Automerge script <automerge@asterisk.org>
2015-03-27 21:17:45 +0000

    Multiple revisions 433617,433620,433623
    ........
      r433617 | rmudgett | 2015-03-27 15:23:58 -0500 (Fri, 27 Mar 2015) | 12 lines
      
      res_pjsip_registrar_expire.c: Cleanup scheduler leaks on unload/shutdown.
      
      Contact expiration object refs were leaked when the module was unloaded.
      
      * Made empty the scheduler of entries before destroying it to release the
      object ref held by the scheduler entry.
      
      Review: https://reviewboard.asterisk.org/r/4523/
      ........
      
      Merged revisions 433596 from http://svn.asterisk.org/svn/asterisk/branches/13
    ........
      r433620 | mmichelson | 2015-03-27 15:46:55 -0500 (Fri, 27 Mar 2015) | 17 lines
      
      Add stateful PJSIP response API call, and use it for out-of-dialog responses.
      
      Asterisk had an issue where retransmissions of MESSAGE requests resulted in
      Asterisk processing the retransmission as if it were a new MESSAGE request.
      
      This patch fixes the issue by creating a transaction in PJSIP on the incoming
      request. This way, if a retransmission arrives, the PJSIP transaction layer
      will resend the response and Asterisk will not ever see the retransmission.
      
      ASTERISK-24920 #close
      Reported by Mark Michelson
      
      Review: https://reviewboard.asterisk.org/r/4532/
      ........
      
      Merged revisions 433619 from http://svn.asterisk.org/svn/asterisk/branches/13
    ........
      r433623 | rmudgett | 2015-03-27 16:06:22 -0500 (Fri, 27 Mar 2015) | 13 lines
      
      res_pjsip_registrar_expire.c: Made use ao2 container template routines and eliminated some RAII_VAR() usage.
      
      * Converted the contact_autoexpire container to use the ao2 template hash
      and cmp functions.  Also made use the OBJ_SEARCH_xxx names instead of the
      deprecated names.
      
      * Eliminates several unnecessary uses of RAII_VAR().
      
      Review: https://reviewboard.asterisk.org/r/4524/
      ........
      
      Merged revisions 433622 from http://svn.asterisk.org/svn/asterisk/branches/13
    ........
    
    Merged revisions 433617,433620,433623 from file:///srv/subversion/repos/asterisk/trunk
    
    
    git-svn-id: https://origsvn.digium.com/svn/asterisk/team/group/dns@433626 65c4cc65-6c06-0410-ace0-fbb531ad65f3

--- res/res_pjsip_registrar_expire.c
+++ res/res_pjsip_registrar_expire.c
@@ -57,32 +57,76 @@ static void contact_expiration_destroy(void *obj)
 /*! \brief Hashing function for contact auto-expiration */
 static int contact_expiration_hash(const void *obj, const int flags)
 {
-	const struct contact_expiration *expiration = obj;
-	const char *id = obj;
+	const struct contact_expiration *object;
+	const char *key;
 
-	return ast_str_hash(flags & OBJ_KEY ? id : ast_sorcery_object_get_id(expiration->contact));
+	switch (flags & OBJ_SEARCH_MASK) {
+	case OBJ_SEARCH_KEY:
+		key = obj;
+		break;
+	case OBJ_SEARCH_OBJECT:
+		object = obj;
+		key = ast_sorcery_object_get_id(object->contact);
+		break;
+	default:
+		/* Hash can only work on something with a full key. */
+		ast_assert(0);
+		return 0;
+	}
+	return ast_str_hash(key);
 }
 
 /*! \brief Comparison function for contact auto-expiration */
 static int contact_expiration_cmp(void *obj, void *arg, int flags)
 {
-	struct contact_expiration *expiration1 = obj, *expiration2 = arg;
-	const char *id = arg;
+	const struct contact_expiration *object_left = obj;
+	const struct contact_expiration *object_right = arg;
+	const char *right_key = arg;
+	int cmp;
 
-	return !strcmp(ast_sorcery_object_get_id(expiration1->contact), flags & OBJ_KEY ? id :
-		       ast_sorcery_object_get_id(expiration2->contact)) ? CMP_MATCH | CMP_STOP : 0;
+	switch (flags & OBJ_SEARCH_MASK) {
+	case OBJ_SEARCH_OBJECT:
+		right_key = ast_sorcery_object_get_id(object_right->contact);
+		/* Fall through */
+	case OBJ_SEARCH_KEY:
+		cmp = strcmp(ast_sorcery_object_get_id(object_left->contact), right_key);
+		break;
+	case OBJ_SEARCH_PARTIAL_KEY:
+		/*
+		 * We could also use a partial key struct containing a length
+		 * so strlen() does not get called for every comparison instead.
+		 */
+		cmp = strncmp(ast_sorcery_object_get_id(object_left->contact), right_key,
+			strlen(right_key));
+		break;
+	default:
+		/*
+		 * What arg points to is specific to this traversal callback
+		 * and has no special meaning to astobj2.
+		 */
+		cmp = 0;
+		break;
+	}
+	if (cmp) {
+		return 0;
+	}
+	/*
+	 * At this point the traversal callback is identical to a sorted
+	 * container.
+	 */
+	return CMP_MATCH;
 }
 
 /*! \brief Scheduler function which deletes a contact */
 static int contact_expiration_expire(const void *data)
 {
-	RAII_VAR(struct contact_expiration *, expiration, (void*)data, ao2_cleanup);
+	struct contact_expiration *expiration = (void *) data;
 
 	expiration->sched = -1;
 
 	/* This will end up invoking the deleted observer callback, which will perform the unlinking and such */
 	ast_sorcery_delete(ast_sip_get_sorcery(), expiration->contact);
-
+	ao2_ref(expiration, -1);
 	return 0;
 }
 
@@ -90,14 +134,16 @@ static int contact_expiration_expire(const void *data)
 static void contact_expiration_observer_created(const void *object)
 {
 	const struct ast_sip_contact *contact = object;
-	RAII_VAR(struct contact_expiration *, expiration, NULL, ao2_cleanup);
+	struct contact_expiration *expiration;
 	int expires = MAX(0, ast_tvdiff_ms(contact->expiration_time, ast_tvnow()));
 
 	if (ast_tvzero(contact->expiration_time)) {
 		return;
 	}
 
-	if (!(expiration = ao2_alloc_options(sizeof(*expiration), contact_expiration_destroy, AO2_ALLOC_OPT_LOCK_NOLOCK))) {
+	expiration = ao2_alloc_options(sizeof(*expiration), contact_expiration_destroy,
+		AO2_ALLOC_OPT_LOCK_NOLOCK);
+	if (!expiration) {
 		return;
 	}
 
@@ -106,39 +152,46 @@ static void contact_expiration_observer_created(const void *object)
 
 	ao2_ref(expiration, +1);
 	if ((expiration->sched = ast_sched_add(sched, expires, contact_expiration_expire, expiration)) < 0) {
-		ao2_cleanup(expiration);
+		ao2_ref(expiration, -1);
 		ast_log(LOG_ERROR, "Scheduled expiration for contact '%s' could not be performed, contact may persist past life\n",
 			ast_sorcery_object_get_id(contact));
-		return;
+	} else {
+		ao2_link(contact_autoexpire, expiration);
 	}
-
-	ao2_link(contact_autoexpire, expiration);
+	ao2_ref(expiration, -1);
 }
 
 /*! \brief Observer callback for when a contact is updated */
 static void contact_expiration_observer_updated(const void *object)
 {
 	const struct ast_sip_contact *contact = object;
-	RAII_VAR(struct contact_expiration *, expiration, ao2_find(contact_autoexpire, ast_sorcery_object_get_id(contact), OBJ_KEY), ao2_cleanup);
+	struct contact_expiration *expiration;
 	int expires = MAX(0, ast_tvdiff_ms(contact->expiration_time, ast_tvnow()));
 
+	expiration = ao2_find(contact_autoexpire, ast_sorcery_object_get_id(contact),
+		OBJ_SEARCH_KEY);
 	if (!expiration) {
 		return;
 	}
 
-	AST_SCHED_REPLACE_UNREF(expiration->sched, sched, expires, contact_expiration_expire, expiration, ao2_cleanup(expiration), ao2_cleanup(expiration), ao2_ref(expiration, +1));
+	AST_SCHED_REPLACE_UNREF(expiration->sched, sched, expires, contact_expiration_expire,
+		expiration, ao2_cleanup(expiration), ao2_cleanup(expiration), ao2_ref(expiration, +1));
+	ao2_ref(expiration, -1);
 }
 
 /*! \brief Observer callback for when a contact is deleted */
 static void contact_expiration_observer_deleted(const void *object)
 {
-	RAII_VAR(struct contact_expiration *, expiration, ao2_find(contact_autoexpire, ast_sorcery_object_get_id(object), OBJ_KEY | OBJ_UNLINK), ao2_cleanup);
+	struct contact_expiration *expiration;
 
+	expiration = ao2_find(contact_autoexpire, ast_sorcery_object_get_id(object),
+		OBJ_SEARCH_KEY | OBJ_UNLINK);
 	if (!expiration) {
 		return;
 	}
 
 	AST_SCHED_DEL_UNREF(sched, expiration->sched, ao2_cleanup(expiration));
+	ao2_ref(expiration, -1);
 }
 
 /*! \brief Observer callbacks for autoexpiring contacts */
@@ -166,65 +219,78 @@ static int contact_expiration_setup(void *obj, void *arg, int flags)
 /*! \brief Initialize auto-expiration of any existing contacts */
 static void contact_expiration_initialize_existing(void)
 {
-	RAII_VAR(struct ao2_container *, contacts, NULL, ao2_cleanup);
+	struct ao2_container *contacts;
 
-	if (!(contacts = ast_sorcery_retrieve_by_fields(ast_sip_get_sorcery(), "contact", AST_RETRIEVE_FLAG_MULTIPLE | AST_RETRIEVE_FLAG_ALL, NULL))) {
+	contacts = ast_sorcery_retrieve_by_fields(ast_sip_get_sorcery(), "contact",
+		AST_RETRIEVE_FLAG_MULTIPLE | AST_RETRIEVE_FLAG_ALL, NULL);
+	if (!contacts) {
 		return;
 	}
 
 	ao2_callback(contacts, OBJ_NODATA, contact_expiration_setup, NULL);
+	ao2_ref(contacts, -1);
+}
+
+static int unload_observer_delete(void *obj, void *arg, int flags)
+{
+	struct contact_expiration *expiration = obj;
+
+	AST_SCHED_DEL_UNREF(sched, expiration->sched, ao2_cleanup(expiration));
+	return CMP_MATCH;
+}
+
+static int unload_module(void)
+{
+	ast_sorcery_observer_remove(ast_sip_get_sorcery(), "contact", &contact_expiration_observer);
+	if (sched) {
+		ao2_callback(contact_autoexpire, OBJ_MULTIPLE | OBJ_NODATA | OBJ_UNLINK,
+			unload_observer_delete, NULL);
+		ast_sched_context_destroy(sched);
+		sched = NULL;
+	}
+	ao2_cleanup(contact_autoexpire);
+	contact_autoexpire = NULL;
+
+	return 0;
 }
 
 static int load_module(void)
 {
 	CHECK_PJSIP_MODULE_LOADED();
 
-	if (!(contact_autoexpire = ao2_container_alloc_options(AO2_ALLOC_OPT_LOCK_NOLOCK, CONTACT_AUTOEXPIRE_BUCKETS,
-		contact_expiration_hash, contact_expiration_cmp))) {
+	contact_autoexpire = ao2_container_alloc_options(AO2_ALLOC_OPT_LOCK_NOLOCK,
+		CONTACT_AUTOEXPIRE_BUCKETS, contact_expiration_hash, contact_expiration_cmp);
+	if (!contact_autoexpire) {
 		ast_log(LOG_ERROR, "Could not create container for contact auto-expiration\n");
 		return AST_MODULE_LOAD_FAILURE;
 	}
 
 	if (!(sched = ast_sched_context_create())) {
 		ast_log(LOG_ERROR, "Could not create scheduler for contact auto-expiration\n");
-		goto error;
+		unload_module();
+		return AST_MODULE_LOAD_FAILURE;
 	}
 
 	if (ast_sched_start_thread(sched)) {
 		ast_log(LOG_ERROR, "Could not start scheduler thread for contact auto-expiration\n");
-		goto error;
+		unload_module();
+		return AST_MODULE_LOAD_FAILURE;
 	}
 
 	contact_expiration_initialize_existing();
 
 	if (ast_sorcery_observer_add(ast_sip_get_sorcery(), "contact", &contact_expiration_observer)) {
 		ast_log(LOG_ERROR, "Could not add observer for notifications about contacts for contact auto-expiration\n");
-		goto error;
+		unload_module();
+		return AST_MODULE_LOAD_FAILURE;
 	}
 
 	return AST_MODULE_LOAD_SUCCESS;
-
-error:
-	if (sched) {
-		ast_sched_context_destroy(sched);
-	}
-
-	ao2_cleanup(contact_autoexpire);
-	return AST_MODULE_LOAD_FAILURE;
-}
-
-static int unload_module(void)
-{
-	ast_sorcery_observer_remove(ast_sip_get_sorcery(), "contact", &contact_expiration_observer);
-	ast_sched_context_destroy(sched);
-	ao2_cleanup(contact_autoexpire);
-
-	return 0;
 }
 
 AST_MODULE_INFO(ASTERISK_GPL_KEY, AST_MODFLAG_LOAD_ORDER, "PJSIP Contact Auto-Expiration",
-		.support_level = AST_MODULE_SUPPORT_CORE,
-		.load = load_module,
-		.unload = unload_module,
-		.load_pri = AST_MODPRI_APP_DEPEND,
-	       );
+	.support_level = AST_MODULE_SUPPORT_CORE,
+	.load = load_module,
+	.unload = unload_module,
+	.load_pri = AST_MODPRI_APP_DEPEND,
+	);
