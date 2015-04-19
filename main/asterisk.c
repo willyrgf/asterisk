481ea76d13600c19828c6dc919edc487d9bf470f
Matt Jordan <mjordan@digium.com>
2015-04-19 17:34:49 -0500

    Add an awesome feature. Sorta.
    Change-Id: I6c051152462238ef628d2ab59d17d1eea910fadc

--- main/asterisk.c
+++ main/asterisk.c
@@ -1,7 +1,7 @@
 /*
  * Asterisk -- An open source telephony toolkit.
  *
- * Copyright (C) 1999 - 2014, Digium, Inc.
+ * Copyright (C) 1999 - 2015, Digium, Inc.
  *
  * Mark Spencer <markster@digium.com>
  *
